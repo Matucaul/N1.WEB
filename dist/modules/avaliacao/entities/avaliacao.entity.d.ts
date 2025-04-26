75256296b9ab5ac508d2b669300d7e9eca0a6cfd
Louie Nery <louie.nery1500@gmail.com>
2025-04-26 10:26:52 -0300

    Add files via upload

---
+++ dist/modules/avaliacao/entities/avaliacao.entity.d.ts
@@ -0,0 +1,9 @@
+export declare class Avaliacao {
+    id: string;
+    clienteId: string;
+    pedidoId: string;
+    nota: number;
+    comentario: string;
+    data: Date;
+    generateID(): void;
+}
